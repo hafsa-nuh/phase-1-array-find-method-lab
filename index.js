04c661cc3867964c712a9ea21b467a1794eed9ab
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-18 14:15:23 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+// function superbowlWin(array){
+//     const result = array.find(function(){
+//         array.result === 'W'
+//     })
+//     return !! result ? array.year : undefined;
+// }
+function superbowlWin(record){
+    const result = record.find(record => record.result === 'W')
+    return !! result ? result.year : undefined;
+}
